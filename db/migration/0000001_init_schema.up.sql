1df78643234815036e5adba6a9d3098f6bb9e8fe
borntodie-new <2631937454@qq.com>
2023-06-25 16:56:30 +0800

    恢复到上一个版本

--- db/migration/0000001_init_schema.up.sql
+++ db/migration/0000001_init_schema.up.sql
@@ -1,12 +1,3 @@
-CREATE TABLE "users" (
-                         "username" varchar PRIMARY KEY,
-                         "hashed_password" varchar NOT NULL,
-                         "full_name" varchar NOT NULL,
-                         "email" varchar UNIQUE NOT NULL,
-                         "created_at" timestamptz NOT NULL DEFAULT (now()),
-                         "password_changed_at" timestamptz NOT NULL DEFAULT ('0001-01-01 00:00:00+00')
-);
-
 CREATE TABLE "accounts" (
                             "id" bigserial PRIMARY KEY,
                             "owner" varchar NOT NULL,
@@ -17,22 +8,20 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "entries" (
                            "id" bigserial PRIMARY KEY,
-                           "account_id" bigint,
+                           "account_id" bigint NOT NULL,
                            "amount" bigint NOT NULL,
                            "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
                              "id" bigserial PRIMARY KEY,
-                             "from_account_id" bigint,
-                             "to_account_id" bigint,
+                             "from_account_id" bigint NOT NULL,
+                             "to_account_id" bigint NOT NULL,
                              "amount" bigint NOT NULL,
                              "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE INDEX ON "accounts" ("owner");
-
-CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
 
 CREATE INDEX ON "entries" ("account_id");
 
@@ -45,8 +34,6 @@ CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
-
-ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 
 ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
