04d5effa31cf94bbab39e7b8e2b001d9205db823
borntodie-new <2631937454@qq.com>
2023-06-26 22:42:33 +0800

    修改sessions表字段信息

--- db/migration/000003_add_sessions.up.sql
+++ db/migration/000003_add_sessions.up.sql
@@ -6,8 +6,7 @@ CREATE TABLE "sessions"
     "user_agent"          varchar     NOT NULL,
     "client_ip"           varchar     NOT NULL,
     "is_block"            boolean     NOT NULL DEFAULT false,
-    "expires_at"          timestamptz NOT NULL,
-    "password_changed_at" timestamptz NOT NULL DEFAULT ('0001-01-01 00:00:00+00')
+    "expires_at"          timestamptz NOT NULL
 );
 
 ALTER TABLE "sessions"
